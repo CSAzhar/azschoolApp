6e139ca057f575d31950962d864e67b28f38d8f5
CSAzhar <azhardarampur@gmail.com>
2025-05-30 14:27:57 +0530

    added log file and banner template

--- azschool/src/main/java/com/azschool/controller/SchoolController.java
+++ azschool/src/main/java/com/azschool/controller/SchoolController.java
@@ -1,5 +1,7 @@
 package com.azschool.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,8 +24,11 @@ public class SchoolController {
 	@Autowired
 	private SchoolService schoolService;
 	
+	private static final Logger logger = LoggerFactory.getLogger(SchoolController.class);
+	
 	@GetMapping("/all")
 	public ResponseEntity<Response> getAllSchool(){
+		logger.info("inside getting school controller and getting all schools");
 		Response response = schoolService.getAllSchools();
 		return ResponseEntity.status(response.getStatusCode()).body(response);
 	}
